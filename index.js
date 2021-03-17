2bc2e10466ecf2c69c7325385c20d48d3181dc99
kswon-arch <kswon@gemiso.com>
2021-03-17 18:00:41 +0900

    rb0
    rb1
    
    rb2

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ import backbone from 'backbone'
 
     window.HELP_IMPROVE_VIDEOJS = false
 
-    console.log('git rebase -i HEAD 01')
-    console.log('git rebase -i HEAD 02')
-    console.log('git rebase -i HEAD 03')
+    console.log('rb2')
 
     // console.log(path.join(__dirname, './'))
 
